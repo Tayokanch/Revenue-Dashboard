deed9b7e070d4e49bff054e8816b187737cff1d2
Tayokanch <tayoquadri11@gmail.com>
2025-04-27 11:39:46 +0100

    style updated

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -15,11 +15,11 @@ const OverviewPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex-1 overflow-auto relatibe z-20">
+    <div className="flex-1 overflow-auto relative z-20">
       <Header title="Financial Overview" />
       <main className="max-w-8xl mx-auto py-6 px-4 lg:px-8 xl:px-20">
         <motion.div
-          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 mb-8"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
